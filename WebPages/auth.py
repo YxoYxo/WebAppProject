fb00413379b4c4c207c85b3e076f6f8a74aad516
Yusuf.Oyeleye <y.oyeleye@domosfs.com>
2024-03-20 15:40:18 +0000

    moving forward with html rendering

---
+++ WebPages/auth.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login')
+def login():
+    return "<p>Login</>"
+
+@auth.route('/register')
+def register():
+    return render_template("register.html")
+
+
