330f7de681e047466b14237530fd12eaf8f33f31
Yusuf.Oyeleye <y.oyeleye@domosfs.com>
2024-03-20 16:57:32 +0000

    form entries using post (request from html page)

--- WebPages/auth.py
+++ WebPages/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
@@ -9,6 +9,18 @@ def login():
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        companyName = request.form.get('companyName')
+        streetAdd = request.form.get('streetAdd')
+        city = request.form.get('city')
+        state = request.form.get('state')
+        zipCode = request.form.get('zipCode')
+        country = request.form.get('country')
+        email = request.form.get('email')
+        contactNum = request.form.get('contactNum')
+        
     return render_template("register.html")
 
 
